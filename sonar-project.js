808a40329041c20b2d9682f7f2547e356ce5e7b7
Elhope02 <111543622+Elhope02@users.noreply.github.com>
2022-10-07 12:12:47 -0400

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,18 +1,18 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://172.31.27.43:9000/',
-       options : {
-	    'sonar.projectDescription': 'This is a Node JS application',
-	    'sonar.projectName': 'Node JS Application - Sample',
+  serverUrl:'http://172.31.27.43:9000/',
+       options:{
+	    'sonar.projectDescription':'This is a Node JS application',
+	    'sonar.projectName':'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsLandmarkTechnologies',
-	    'sonar.login': '23c700352f9e94e243b3cf3ed82dea561d968b6c',
-	    //'sonar.password': 'admin',
+	    'sonar.login':'a340bbcafb2ec4fceb9effe4f67c0234ef961eeb',
+	    //'sonar.password':'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
             'sonar.sources': '.',
-	  //'sonar.tests': 'specs',
-          //'sonar.inclusions' : 'src/**'
+	  //'sonar.tests':'specs',
+          //'sonar.inclusions':'src/**'
        },
 }, () => {});
